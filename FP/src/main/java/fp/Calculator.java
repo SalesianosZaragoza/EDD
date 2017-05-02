fbcf1ed0c7cdaa5d04d4074780e085878f70f4e7
sergiosanz <sanzestradasergio@gmail.com>
2017-05-02 09:03:53 +0200

    Primer Commit
    No me entere de que esta rubrica habia que tenerla antes del 2 de mayo,
    por que me baso en las fechas del campus, asique estaré trabajando entre
    hoy y mañana con la rubrica para tenerla cuanto antes

--- FP/src/main/java/fp/Calculator.java
+++ FP/src/main/java/fp/Calculator.java
@@ -2,46 +2,67 @@ package fp;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
 
-
-	/*
-     * este metodo devuelve el Class del object que le pasamos
-     */
 	public static Class classTypeOf(Object x) {
-		throw  new NotImplementedException();
+		return x.getClass();
 	}
 
-
-	/*
-     * devuelve una lista con los n números de la serie de fibonacci.
-     */
 	public static List<Integer> fibonacci(int n) {
-		throw  new NotImplementedException();
+		List<Integer> lista = new ArrayList<Integer>();
+		Integer x = 0, y = 1;
+		Integer resultado;
+		lista.add(y);
+
+		for (int i = 1; i < n; i++) {
+			resultado = x + y;
+			x = y;
+			y = resultado;
+			lista.add(resultado);
+		}
+		return lista;
 	}
 
-	/*
-	 * Escribir todos los números del number al 0 de step en step.
-	 */
 	public static int[] stepThisNumber(int number, int step) {
-		throw  new NotImplementedException();
+		if (step == 0) {
+			int numeros[] = { 0 };
+			return numeros;
+		} else {
+			int numeros[];
+
+			if (number % step == 0) {
+				numeros = new int[(number) / step - 1];
+			} else {
+				numeros = new int[(number) / step];
+			}
+
+			int i = number, j = 0;
+			while ((i - step) > (0)) {
+				i -= step;
+				numeros[j] = i;
+				j++;
+			}
+			return numeros;
 		}
+	}
 
 	/*
 	 * Módulo al que se le pasa un número entero del 0 al 20 y devuelve los
 	 * divisores que tiene.
 	 */
 	public static int[] divisors(int n) {
-		throw  new NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	/*
-	 * Toma como parámetros una cadena de caracteres y devuelve cierto si la cadena resulta ser un palíndromo
+	 * Toma como parámetros una cadena de caracteres y devuelve cierto si la
+	 * cadena resulta ser un palíndromo
 	 */
 	public static boolean checkIsPalindrome(String cadena) {
-		throw  new NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	/*
@@ -49,7 +70,7 @@ public class Calculator {
 	 * mostrar: cincuenta y seis
 	 */
 	public static String speakToMe(int n) {
-		throw  new NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	/*
@@ -57,13 +78,13 @@ public class Calculator {
 	 * dd-MM-yyyy
 	 */
 	public static boolean isLeapYear(String fecha) {
-		throw  new NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	/*
 	 * este metodo devuelve cierto si la fecha es válida
 	 */
 	public static boolean isValidDate(String date) {
-		throw  new NotImplementedException();
+		throw new NotImplementedException();
 	}
 }
