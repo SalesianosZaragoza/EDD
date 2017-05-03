e84eb75264942e0e5a6cd6a4fc345024b57e480c
Daniel <3ddanielangel@gmail.com>
2017-05-03 21:04:24 +0200

    implementando los 4 primeros metodos

--- FP/src/main/java/fp/Calculator.java
+++ FP/src/main/java/fp/Calculator.java
@@ -2,6 +2,7 @@ package fp;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
@@ -11,7 +12,7 @@ public class Calculator {
      * este metodo devuelve el Class del object que le pasamos
      */
 	public static Class classTypeOf(Object x) {
-		throw  new NotImplementedException();
+		return x.getClass();
 	}
 
 
@@ -19,23 +20,68 @@ public class Calculator {
      * devuelve una lista con los n números de la serie de fibonacci.
      */
 	public static List<Integer> fibonacci(int n) {
-		throw  new NotImplementedException();
+		List<Integer> lista = new ArrayList<Integer>();
+		Integer x = 0, y = 1;
+		Integer result;
+		lista.add(y);
+		 
+		for (int i = 1; i < n; i++) {
+		 	result = x + y;
+			x = y;
+		 	y = result;
+		 	lista.add(result);
+		}
+		return lista;
 	}
 
 	/*
 	 * Escribir todos los números del number al 0 de step en step.
 	 */
 	public static int[] stepThisNumber(int number, int step) {
-		throw  new NotImplementedException();
-		}
+		if (step == 0) {
+			int numeros[] = { 0 };
+			return numeros;
+		}else{
+				int numeros[];
+			 	if (number % step == 0) {
+			 		numeros = new int[(number) / step - 1];
+			 	}else{
+			 		numeros = new int[(number) / step];
+			 	}
+			 
+			 	int i = number, j = 0;
+			 	while ((i - step) > (0)) {
+			 			i -= step;
+			 			numeros[j] = i;
+			 			j++;
+			 			}
+			 	return numeros;
+			}
+	}
 
 	/*
 	 * Módulo al que se le pasa un número entero del 0 al 20 y devuelve los
 	 * divisores que tiene.
 	 */
 	public static int[] divisors(int n) {
-		throw  new NotImplementedException();
+		if (n == 0)
+			return null;
+			else {
+				int divisores[] = new int[n];
+				int j = 0;
+				for (int i = n; i > 0; i--)
+					if (n % i == 0) {
+						divisores[j] = i;
+						j++;
+			}
+				int result[] = new int[j];
+				for (int x = 0; x < j; x++)
+					result[x] = divisores[x];
+				return result;
+			}
+			 
 	}
+	
 
 	/*
 	 * Toma como parámetros una cadena de caracteres y devuelve cierto si la cadena resulta ser un palíndromo
