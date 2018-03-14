fe4eb78665c5c1aae7577cabb4e4405e300bedd4
TeresaGarcia <teresagar181@gmail.com>
2018-03-14 19:14:28 +0100

    Primer test resuelto
    Signed-off-by: TeresaGarcia <teresagar181@gmail.com>

--- FP/src/main/java/fp/Calculator.java
+++ FP/src/main/java/fp/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator {
      * este metodo devuelve el Class del object que le pasamos
      */
 	public static Class classTypeOf(Object x) {
-		throw  new NotImplementedException();
+		return x.getClass();
 	}
 
 
