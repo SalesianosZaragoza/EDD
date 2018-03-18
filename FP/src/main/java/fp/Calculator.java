9fbdd624ab9adc3c31cdd121f9d1f7b2755a7125
TeresaGarcia <teresagar181@gmail.com>
2018-03-18 16:09:10 +0100

    Test palíndromos
    Signed-off-by: TeresaGarcia <teresagar181@gmail.com>

--- FP/src/main/java/fp/Calculator.java
+++ FP/src/main/java/fp/Calculator.java
@@ -2,54 +2,69 @@ package fp;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
 
-
 	/*
-     * este metodo devuelve el Class del object que le pasamos
-     */
+	 * este metodo devuelve el Class del object que le pasamos
+	 */
 	public static Class classTypeOf(Object x) {
 		return x.getClass();
 	}
 
-
 	/*
-     * devuelve una lista con los n números de la serie de fibonacci.
-     */
+	 * devuelve una lista con los n números de la serie de fibonacci.
+	 */
 	public static List<Integer> fibonacci(int n) {
-		throw  new NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	/*
 	 * Escribir todos los números del number al 0 de step en step.
 	 */
 	public static int[] stepThisNumber(int number, int step) {
-		throw  new NotImplementedException();
-		}
+		throw new NotImplementedException();
+	}
 
 	/*
 	 * Módulo al que se le pasa un número entero del 0 al 20 y devuelve los
 	 * divisores que tiene.
 	 */
 	public static int[] divisors(int n) {
-		throw  new NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	/*
-	 * Toma como parámetros una cadena de caracteres y devuelve cierto si la cadena resulta ser un palíndromo
+	 * Toma como parámetros una cadena de caracteres y devuelve cierto si la cadena
+	 * resulta ser un palíndromo
 	 */
 	public static boolean checkIsPalindrome(String cadena) {
-		throw  new NotImplementedException();
+		String vocalesASustituir[] = { "[àá]", "[éè]", "í", "[óò]", "[úü]" };
+		String vocalesSustitutas[] = { "a", "e", "i", "o", "u" };
+		if (cadena == null)
+			return false;
+		else {
+			for (int i = 0; i < vocalesSustitutas.length; i++)
+				cadena = cadena.replaceAll(vocalesASustituir[i], vocalesSustitutas[i]);
+			cadena = cadena.toLowerCase().replaceAll("\\W", "");
+			int n = cadena.length();
+			for (int i = 0; i < (n / 2); i++) {
+				if (cadena.charAt(i) != cadena.charAt(n - i - 1)) {
+					return false;
+				}
+			}
+			return true;
+		}
 	}
 
 	/*
-	 * Pedir un número de 0 a 99 y mostrarlo escrito. Por ejemplo, para 56
-	 * mostrar: cincuenta y seis
+	 * Pedir un número de 0 a 99 y mostrarlo escrito. Por ejemplo, para 56 mostrar:
+	 * cincuenta y seis
 	 */
 	public static String speakToMe(int n) {
-		throw  new NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	/*
@@ -57,13 +72,13 @@ public class Calculator {
 	 * dd-MM-yyyy
 	 */
 	public static boolean isLeapYear(String fecha) {
-		throw  new NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	/*
 	 * este metodo devuelve cierto si la fecha es válida
 	 */
 	public static boolean isValidDate(String date) {
-		throw  new NotImplementedException();
+		throw new NotImplementedException();
 	}
 }
