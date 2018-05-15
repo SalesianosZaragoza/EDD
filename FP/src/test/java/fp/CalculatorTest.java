02181c81638bfca167d90e6d690cfd01553b85f7
Guillermo Ye <34741431+GuillermoYe@users.noreply.github.com>
2018-05-15 13:19:00 +0200

    Primera entrega Guillermo Ye

--- FP/src/test/java/fp/CalculatorTest.java
+++ FP/src/test/java/fp/CalculatorTest.java
@@ -1,6 +1,5 @@
 package fp;
 
-
 import static fp.Calculator.checkIsPalindrome;
 import static fp.Calculator.classTypeOf;
 import static fp.Calculator.divisors;
@@ -18,12 +17,9 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
-
 import org.junit.Test;
 
-
 public class CalculatorTest {
-
 
 	@Test
 	public void testClassType() {
@@ -57,22 +53,14 @@ public class CalculatorTest {
 	@Test
 	public void testIsPalindrome() {
 		assertNotNull(checkIsPalindrome(null));
-		assertTrue(
-checkIsPalindrome("No Mara, sometamos o matemos a Ramón."));
-		assertTrue(
-checkIsPalindrome("¿Acaso hubo búhos acá?"));
-		assertTrue(
-checkIsPalindrome("No lata, no: la totalidad arada dilato talón a talón."));
-		assertTrue(
-checkIsPalindrome("Allí, tieta Mercè, faci cafè, crema, te i til·la"));
-		assertTrue(
-checkIsPalindrome("No Mara, sometamos o matemos a Ramón."));
-		assertFalse(
-				checkIsPalindrome("Buena suerte con los Test"));
-		assertFalse(
-checkIsPalindrome("4ª ley de kepler: gazpacho y mochilo siempre van con pincho"));
-		assertFalse(
-checkIsPalindrome("No me guardéis rencor, al menos no mucho"));
+		assertTrue(checkIsPalindrome("No Mara, sometamos o matemos a Ramón."));
+		assertTrue(checkIsPalindrome("¿Acaso hubo búhos acá?"));
+		assertTrue(checkIsPalindrome("No lata, no: la totalidad arada dilato talón a talón."));
+		assertTrue(checkIsPalindrome("Allí, tieta Mercè, faci cafè, crema, te i til·la"));
+		assertTrue(checkIsPalindrome("No Mara, sometamos o matemos a Ramón."));
+		assertFalse(checkIsPalindrome("Buena suerte con los Test"));
+		assertFalse(checkIsPalindrome("4ª ley de kepler: gazpacho y mochilo siempre van con pincho"));
+		assertFalse(checkIsPalindrome("No me guardéis rencor, al menos no mucho"));
 		System.out.println("2P");
 	}
 
@@ -115,7 +103,6 @@ checkIsPalindrome("No me guardéis rencor, al menos no mucho"));
 		assertFalse(isValidDate("01-01-0000"));
 		System.out.println("1P");
 	}
-
 
 	@Test
 	public void testFibonacci() {
