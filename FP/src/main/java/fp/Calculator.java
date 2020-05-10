465576424db2c9e2b093461958b59e5951b3e2e9
cristiancardonamarin <cristiancardonamarin@gmail.com>
2020-05-10 17:16:15 +0200

    He tenido problemas con git, asi que los commits que habia hecho los he borrado y paso todo en un commit

--- FP/src/main/java/fp/Calculator.java
+++ FP/src/main/java/fp/Calculator.java
@@ -1,5 +1,8 @@
 package fp;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
@@ -11,7 +14,8 @@ public class Calculator {
      * este metodo devuelve el Class del object que le pasamos
      */
 	public static Class classTypeOf(Object x) {
-		throw  new NotImplementedException();
+		Class hola = x.getClass();
+        return hola;
 	}
 
 
@@ -19,7 +23,16 @@ public class Calculator {
      * devuelve una lista con los n números de la serie de fibonacci.
      */
 	public static List<Integer> fibonacci(int n) {
-		throw new NotImplementedException();
+		 List<Integer> a = new ArrayList<Integer>();
+	        int cont = 1;
+	        a.add(cont);
+
+	        for (int i = 1; i < n; i++){
+	            int anterior = a.get(i - 1);
+	            a.add(cont);
+	            cont = anterior + cont;
+	        }
+	        return a;
 	}
 
 	/*
@@ -34,14 +47,35 @@ public class Calculator {
 	 * divisores que tiene.
 	 */
 	public static int[] divisors(int n) {
-		throw  new NotImplementedException();
+		int auxiliar = 0;
+        if(n==0) {
+            return null; 
+        }
+        for (int i = 1 ; i <= n ; i++)
+            if (n % i == 0)
+                auxiliar++;
+        int []divisores = new int[auxiliar];
+        for (int i = n, j = 0 ; i >0 ; i--)
+            if (n % i == 0) {
+                divisores[j] = i;
+                j++;
+            }
+        return divisores;
 	}
 
 	/*
 	 * Toma como parámetros una cadena de caracteres y devuelve cierto si la cadena resulta ser un palíndromo
 	 */
 	public static boolean checkIsPalindrome(String cadena) {
-		throw  new NotImplementedException();
+		 if (cadena == null) {
+	            return false;
+	        }else {
+	            cadena = cadena.toLowerCase().replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o")
+	                    .replace("ú", "u").replace(" ", "").replace(".", "").replace(",", "").replace("?", "").replace("¿","")
+	                    .replace(":", "").replace("·","");
+	            String invertir = new StringBuilder(cadena).reverse().toString();
+	            return invertir.equals(cadena);
+	        }
 	}
 
 	/*
@@ -57,13 +91,29 @@ public class Calculator {
 	 * dd-MM-yyyy
 	 */
 	public static boolean isLeapYear(String fecha) {
-		throw  new NotImplementedException();
+		  if(fecha=="") {
+	            return false;
+	        }
+	        String year = fecha.substring(fecha.length()-4);
+	        int anio = Integer.parseInt(year);
+	        if ((anio % 4 == 0) && ((anio % 100 != 0) || (anio % 400 == 0)))
+	            return true;
+	        else
+	            return false;
 	}
 
 	/*
 	 * este metodo devuelve cierto si la fecha es válida
 	 */
 	public static boolean isValidDate(String date) {
-		throw  new NotImplementedException();
+		try {
+            SimpleDateFormat formatoFecha = new SimpleDateFormat("dd-MM-yyyy");
+            formatoFecha.setLenient(false);
+            formatoFecha.parse(date);
+        } catch (ParseException e) {
+            return false;
+        }
+        return true;
+    
 	}
 }
