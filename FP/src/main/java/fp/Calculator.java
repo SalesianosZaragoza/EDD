a0982e3cf1089ef15570dc05971625d2c5f74da3
Kerjox <kerjoxgamer095@gmail.com>
2021-03-27 11:14:27 +0100

    Solve error "package sun.reflect.generics.reflectiveObjects does not exist", replaced for "java.lang.UnsupportedOperationException"

--- FP/src/main/java/fp/Calculator.java
+++ FP/src/main/java/fp/Calculator.java
@@ -2,7 +2,7 @@ package fp;
 
 import java.util.List;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.lang.UnsupportedOperationException;
 
 public class Calculator {
 
@@ -11,7 +11,7 @@ public class Calculator {
      * este metodo devuelve el Class del object que le pasamos
      */
 	public static Class classTypeOf(Object x) {
-		throw  new NotImplementedException();
+		throw new UnsupportedOperationException(); 
 	}
 
 
@@ -19,29 +19,29 @@ public class Calculator {
      * devuelve una lista con los n números de la serie de fibonacci.
      */
 	public static List<Integer> fibonacci(int n) {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException(); 
 	}
 
 	/*
 	 * Escribir todos los números del number al 0 de step en step.
 	 */
 	public static int[] stepThisNumber(int number, int step) {
-		throw  new NotImplementedException();
-		}
+		throw new UnsupportedOperationException(); 
+	}
 
 	/*
 	 * Módulo al que se le pasa un número entero del 0 al 20 y devuelve los
 	 * divisores que tiene.
 	 */
 	public static int[] divisors(int n) {
-		throw  new NotImplementedException();
+		throw new UnsupportedOperationException(); 
 	}
 
 	/*
 	 * Toma como parámetros una cadena de caracteres y devuelve cierto si la cadena resulta ser un palíndromo
 	 */
 	public static boolean checkIsPalindrome(String cadena) {
-		throw  new NotImplementedException();
+		throw  new UnsupportedOperationException();
 	}
 
 	/*
@@ -49,7 +49,7 @@ public class Calculator {
 	 * mostrar: cincuenta y seis
 	 */
 	public static String speakToMe(int n) {
-		throw  new NotImplementedException();
+		throw  new UnsupportedOperationException();
 	}
 
 	/*
@@ -57,13 +57,13 @@ public class Calculator {
 	 * dd-MM-yyyy
 	 */
 	public static boolean isLeapYear(String fecha) {
-		throw  new NotImplementedException();
+		throw  new UnsupportedOperationException();
 	}
 
 	/*
 	 * este metodo devuelve cierto si la fecha es válida
 	 */
 	public static boolean isValidDate(String date) {
-		throw  new NotImplementedException();
+		throw  new UnsupportedOperationException();
 	}
 }
