70274537e1a6678b16ad0afaf471d0cb2b03debd
Sergio <serguipe28@gmail.com>
2021-05-22 11:10:08 +0200

    First changes in Calculator

--- FP/src/main/java/fp/Calculator.java
+++ FP/src/main/java/fp/Calculator.java
@@ -1,47 +1,80 @@
 package fp;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class Calculator {
-
-
 	/*
-     * este metodo devuelve el Class del object que le pasamos
-     */
+	 * este metodo devuelve el Class del object que le pasamos
+	 */
 	public static Class classTypeOf(Object x) {
-		throw  new NotImplementedException();
+		return x.getClass();
 	}
 
 
 	/*
-     * devuelve una lista con los n números de la serie de fibonacci.
-     */
+	 * devuelve una lista con los n números de la serie de fibonacci.
+	 */
 	public static List<Integer> fibonacci(int n) {
-		throw new NotImplementedException();
+		List<Integer> fibonnaciList = new ArrayList<Integer>();
+		Integer n1 = 0, n2 = 1;
+		fibonnaciList.add(1);
+
+		for (int i = 0; i < n - 1; i++) {
+			Integer summa = n1 + n2;
+			n1 = n2;
+			n2 = summa;
+			fibonnaciList.add(summa);
+		}
+
+		return fibonnaciList;
 	}
 
 	/*
 	 * Escribir todos los números del number al 0 de step en step.
 	 */
 	public static int[] stepThisNumber(int number, int step) {
-		throw  new NotImplementedException();
+		List<Integer> cadena = new ArrayList<>();
+		while (number - step > 0){
+			cadena.add(number = number - step);
 		}
+		return Conversor(cadena);
+	}
+
+	public static int[] Conversor(List<Integer> list) {
+		int[] cadena = new int[list.size()];
+		for (int i=0; i< list.size(); i++){
+			cadena[i]= list.get(i);
+		}
+		return cadena;
+	}
 
 	/*
 	 * Módulo al que se le pasa un número entero del 0 al 20 y devuelve los
 	 * divisores que tiene.
 	 */
 	public static int[] divisors(int n) {
-		throw  new NotImplementedException();
+		List<Integer> list = new ArrayList<>();
+		if (n == 0)
+			return null;
+		if (n >= 0 && n <= 20) {
+			int counter = 0;
+			for (int i = 20; i > 0; i--) {
+				if (n % i == 0) {
+					list.add(i);
+				}
+			}
+
+			return Conversor(list);
+		}
+		return new int[]{-1};
 	}
 
 	/*
 	 * Toma como parámetros una cadena de caracteres y devuelve cierto si la cadena resulta ser un palíndromo
 	 */
 	public static boolean checkIsPalindrome(String cadena) {
-		throw  new NotImplementedException();
 	}
 
 	/*
@@ -49,7 +82,6 @@ public class Calculator {
 	 * mostrar: cincuenta y seis
 	 */
 	public static String speakToMe(int n) {
-		throw  new NotImplementedException();
 	}
 
 	/*
@@ -57,13 +89,12 @@ public class Calculator {
 	 * dd-MM-yyyy
 	 */
 	public static boolean isLeapYear(String fecha) {
-		throw  new NotImplementedException();
+
 	}
 
 	/*
 	 * este metodo devuelve cierto si la fecha es válida
 	 */
 	public static boolean isValidDate(String date) {
-		throw  new NotImplementedException();
 	}
 }
