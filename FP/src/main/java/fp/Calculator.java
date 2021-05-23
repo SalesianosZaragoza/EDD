a59acea19a0bb81ff45635ab84146f41f6ec18ad
Sergio <serguipe28@gmail.com>
2021-05-23 19:57:18 +0200

    CalculatorTest Finished

--- FP/src/main/java/fp/Calculator.java
+++ FP/src/main/java/fp/Calculator.java
@@ -1,5 +1,12 @@
 package fp;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -74,7 +81,30 @@ public class Calculator {
 	/*
 	 * Toma como parámetros una cadena de caracteres y devuelve cierto si la cadena resulta ser un palíndromo
 	 */
-	public static boolean checkIsPalindrome(String cadena) {
+	public static boolean checkIsPalindrome(String _cadena) {
+		if (_cadena == null) {
+			return false;
+		}
+		String cadena = StringUtils.stripAccents(_cadena).toLowerCase();
+		cadena = cadena.replaceAll(" ", "");
+		cadena = cadena.replaceAll("\\.", "");
+		cadena = cadena.replaceAll(",", "");
+		cadena = cadena.replaceAll("\\?", "");
+		cadena = cadena.replaceAll("\\¿", "");
+		cadena = cadena.replaceAll(":", "");
+		cadena = cadena.replaceAll("·", "");
+
+		int i = 0, j = cadena.length() - 1;
+		while (i < j) {
+			if (cadena.charAt(i) != cadena.charAt(j)) {
+				return false;
+			}
+
+			i++;
+			j--;
+		}
+
+		return true;
 	}
 
 	/*
@@ -82,6 +112,95 @@ public class Calculator {
 	 * mostrar: cincuenta y seis
 	 */
 	public static String speakToMe(int n) {
+		int iUnidad = n % 10;
+		n = n / 10;
+		String sTexto = unidadEnTexto(iUnidad);
+		int iDecena = n % 10;
+		n = n / 10;
+
+		if ((iUnidad == 0) && (iDecena > 0)) {
+			sTexto = decenaEnTexto(iDecena);
+		} else if (iDecena == 1) {
+			sTexto = decenas(10 + iUnidad);
+		} else if (iDecena > 1) {
+			sTexto = decenaEnTexto(iDecena) + " y " + sTexto.toLowerCase();
+		}
+		return sTexto;
+	}
+
+	public static String unidadEnTexto (int num){
+		switch (num){
+			case 0:
+				return "Cero";
+			case 1:
+				return "Uno";
+			case 2:
+				return "Dos";
+			case 3:
+				return "Tres";
+			case 4:
+				return "Cuatro";
+			case 5:
+				return "Cinco";
+			case 6:
+				return "Seis";
+			case 7:
+				return "Siete";
+			case 8:
+				return "Ocho";
+			case 9:
+				return "Nueve";
+			default:
+				return "";
+		}
+	}
+	public static String decenaEnTexto(int num) {
+		switch (num) {
+			case 1:
+				return "Diez";
+			case 2:
+				return "Veinte";
+			case 3:
+				return "Treinta";
+			case 4:
+				return "Cuarenta";
+			case 5:
+				return "Cincuenta";
+			case 6:
+				return "Sesenta";
+			case 7:
+				return "Setenta";
+			case 8:
+				return "Ochenta";
+			case 9:
+				return "Noventa";
+			default:
+				return "";
+		}
+	}
+	public static String decenas(int num){
+		switch (num) {
+			case 11:
+				return "Once";
+			case 12:
+				return "Doce";
+			case 13:
+				return "Trece";
+			case 14:
+				return "Catorce";
+			case 15:
+				return "Quince";
+			case 16:
+				return "Dieciseis";
+			case 17:
+				return "Diecisiete";
+			case 18:
+				return "Dieciocho";
+			case 19:
+				return "Diecinueve";
+			default:
+				return "";
+		}
 	}
 
 	/*
@@ -89,12 +208,23 @@ public class Calculator {
 	 * dd-MM-yyyy
 	 */
 	public static boolean isLeapYear(String fecha) {
-
+		if (fecha == "")
+			return false;
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		return LocalDate.parse(fecha, formatter).isLeapYear();
 	}
 
 	/*
 	 * este metodo devuelve cierto si la fecha es válida
 	 */
 	public static boolean isValidDate(String date) {
+		DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		dateFormat.setLenient(false);
+		try {
+			dateFormat.parse(date);
+		} catch (ParseException e) {
+			return false;
+		}
+		return true;
 	}
 }
