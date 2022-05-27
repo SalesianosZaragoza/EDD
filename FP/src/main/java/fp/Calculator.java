ca7e2bd8fc612b74a0d33c2cab9d75894b5c4334
javierguardiola <guardiola.bajav19@zaragoza.salesianos.edu>
2022-05-27 22:05:31 +0200

    ultima final rubrica

--- FP/src/main/java/fp/Calculator.java
+++ FP/src/main/java/fp/Calculator.java
@@ -1,8 +1,14 @@
 package fp;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang3.NotImplementedException;
+import org.apache.commons.lang3.StringUtils;
 public class Calculator {
 
 
@@ -10,7 +16,8 @@ public class Calculator {
      * este metodo devuelve el Class del object que le pasamos
      */
 	public static Class classTypeOf(Object x) {
-		throw new NotImplementedException("no implementado");
+		Class obtenerClase = x.getClass();
+		return obtenerClase;
 	}
 
 
@@ -18,29 +25,83 @@ public class Calculator {
      * devuelve una lista con los n números de la serie de fibonacci.
      */
 	public static List<Integer> fibonacci(int n) {
-		throw new NotImplementedException("no implementado");
+		List<Integer> lista = new ArrayList<Integer>();
+		int numero = 1;
+		lista.add(numero);
+		lista.add(numero);
+		for (int i = 2; i < n; i++) {
+			lista.add(lista.get(i - 1) + lista.get(i - 2));
+		}
+		return lista;
 	}
 
 	/*
 	 * Escribir todos los números del number al 0 de step en step.
 	 */
 	public static int[] stepThisNumber(int number, int step) {
-		throw new NotImplementedException("no implementado");
+		List<Integer> stepList = new ArrayList<>();
+		while (number - step > 0) {
+			stepList.add(number -= step);
 		}
+		return ListToArray(stepList);
+	}
+
+	public static int[] ListToArray(List<Integer> list) {
+		int[] intArray = new int[list.size()];
+		for (int i = 0; i < list.size(); i++) {
+			intArray[i] = list.get(i);
+		}
+		return intArray;
+	}
 
 	/*
 	 * Módulo al que se le pasa un número entero del 0 al 20 y devuelve los
 	 * divisores que tiene.
 	 */
 	public static int[] divisors(int n) {
-		throw new NotImplementedException("no implementado");
+		if (n == 0)
+			return null;
+		List<Integer> divisorsList = new ArrayList<Integer>();
+		if (n >= 0 && n <= 20) {
+			int counter = 0;
+			for (int i = 20; i > 0; i--) {
+				if (n % i == 0) {
+					divisorsList.add(i);
+				}
+			}
+
+			return ListToArray(divisorsList);
+		}
+		return new int[] { -1 };
 	}
 
 	/*
 	 * Toma como parámetros una cadena de caracteres y devuelve cierto si la cadena resulta ser un palíndromo
 	 */
 	public static boolean checkIsPalindrome(String cadena) {
-		throw new NotImplementedException("no implementado");
+		if (cadena == null) {
+			return false;
+		}
+		String cadena2 = StringUtils.stripAccents(cadena).toLowerCase();
+		cadena2 = cadena2.replaceAll(" ", "");
+		cadena2 = cadena2.replaceAll("\\.", "");
+		cadena2 = cadena2.replaceAll(",", "");
+		cadena2 = cadena2.replaceAll("\\?", "");
+		cadena2 = cadena2.replaceAll("\\¿", "");
+		cadena2 = cadena2.replaceAll(":", "");
+		cadena2 = cadena2.replaceAll("·", "");
+
+		int i = 0, j = cadena2.length() - 1;
+		while (i < j) {
+			if (cadena2.charAt(i) != cadena2.charAt(j)) {
+				return false;
+			}
+
+			i++;
+			j--;
+		}
+
+		return true;
 	}
 
 	/*
@@ -48,7 +109,81 @@ public class Calculator {
 	 * mostrar: cincuenta y seis
 	 */
 	public static String speakToMe(int n) {
-		throw new NotImplementedException("no implementado");
+		int valor;
+		int unidades;
+		int decenas;
+		String numeroEscrito = "";
+		unidades = n % 10;
+		decenas = n / 10;
+
+		switch (decenas) {
+		case 0:
+			numeroEscrito = "";
+			break;
+		case 1:
+			numeroEscrito = "Diez";
+			break;
+		case 2:
+			numeroEscrito = "Veinte";
+			break;
+		case 3:
+			numeroEscrito = "Treinta";
+			break;
+		case 4:
+			numeroEscrito = "Cuarenta";
+			break;
+		case 5:
+			numeroEscrito = "Cincuenta";
+			break;
+		case 6:
+			numeroEscrito = "Sesenta";
+			break;
+		case 7:
+			numeroEscrito = "Setenta";
+			break;
+		case 8:
+			numeroEscrito = "Ochenta";
+			break;
+		case 9:
+			numeroEscrito = "Noventa";
+			break;
+		}
+		switch (unidades) {
+		case 0:
+			numeroEscrito = numeroEscrito + "";
+			break;
+		case 1:
+			numeroEscrito = numeroEscrito + "uno";
+			break;
+		case 2:
+			numeroEscrito = numeroEscrito + "dos";
+			break;
+		case 3:
+			numeroEscrito = numeroEscrito + "tres";
+			break;
+		case 4:
+			numeroEscrito = numeroEscrito + "cuatro";
+			break;
+		case 5:
+			numeroEscrito = numeroEscrito + "cinco";
+			break;
+		case 6:
+			numeroEscrito = numeroEscrito + "seis";
+			break;
+		case 7:
+			numeroEscrito = numeroEscrito + "siete";
+			break;
+		case 8:
+			numeroEscrito = numeroEscrito + "ocho";
+			break;
+		case 9:
+			numeroEscrito = numeroEscrito + "nueve";
+			break;
+
+		}
+		if (n == 0)
+			return "Cero";
+		return numeroEscrito;
 	}
 
 	/*
@@ -56,13 +191,23 @@ public class Calculator {
 	 * dd-MM-yyyy
 	 */
 	public static boolean isLeapYear(String fecha) {
-		throw new NotImplementedException("no implementado");
+		if (fecha == "")
+			return false;
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		return LocalDate.parse(fecha, formatter).isLeapYear();
 	}
 
 	/*
 	 * este metodo devuelve cierto si la fecha es válida
 	 */
 	public static boolean isValidDate(String date) {
-		throw new NotImplementedException("no implementado");
+		DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		dateFormat.setLenient(false);
+		try {
+			dateFormat.parse(date);
+		} catch (ParseException e) {
+			return false;
+		}
+		return true;
 	}
 }
