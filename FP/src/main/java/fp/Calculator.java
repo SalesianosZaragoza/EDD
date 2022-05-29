2ee7f4f55cad94c15273e61fc035ab90f9292f08
daniel <tomas.madan21@zaragoza.salesianos.edu>
2022-05-29 23:30:02 +0200

    añadiendo metodo isLeapYear

--- FP/src/main/java/fp/Calculator.java
+++ FP/src/main/java/fp/Calculator.java
@@ -57,14 +57,54 @@ public class Calculator {
 	 * divisores que tiene.
 	 */
 	public static int[] divisors(int n) {
-		throw new NotImplementedException("no implementado");
+		if (n == 0) {
+			return null;
+		}
+		int numVector = 0;
+		for (int i = 1; i <= n; i++)
+			if (n % i == 0)
+				numVector++;
+		int[] divisor = new int[numVector];
+		for (int i = n, j = 0; i > 0; i--)
+			if (n % i == 0) {
+				divisor[j] = i;
+				j++;
+			}
+		return divisor;
 	}
 
 	/*
 	 * Toma como parámetros una cadena de caracteres y devuelve cierto si la cadena resulta ser un palíndromo
 	 */
 	public static boolean checkIsPalindrome(String cadena) {
-		throw new NotImplementedException("no implementado");
+		if (cadena == null) {
+			return false;
+		} else {
+			cadena = cadena.toLowerCase();
+			cadena = cadena.replace("á", "a");
+			cadena = cadena.replace("é", "e");
+			cadena = cadena.replace("í", "i");
+			cadena = cadena.replace("ó", "o");
+			cadena = cadena.replace("ú", "u");
+			cadena = cadena.replace("¿", "");
+			cadena = cadena.replace("?", "");
+			cadena = cadena.replace(":", "");
+			cadena = cadena.replace("·", "");
+			cadena = cadena.replace(",", "");
+			cadena = cadena.replace(".", "");
+			cadena = cadena.replace(" ", "");
+			int fin = cadena.length() - 1;
+			int ini = 0;
+			boolean isPalindrome = true;
+			while (ini < fin) {
+				if (cadena.charAt(ini) != cadena.charAt(fin)) {
+					isPalindrome = false;
+				}
+				ini++;
+				fin--;
+			}
+			return isPalindrome;
+		}
 	}
 
 	/*
@@ -72,7 +112,87 @@ public class Calculator {
 	 * mostrar: cincuenta y seis
 	 */
 	public static String speakToMe(int n) {
-		throw new NotImplementedException("no implementado");
+		if (n == 0) 
+			return "Cero";
+		else {
+			int numFinal;
+			int unidades, decenas;
+			String fraseFinal = "";
+			numFinal = n;
+			unidades = numFinal % 10;
+			decenas = numFinal / 10;
+			switch (decenas) {
+				case 0:
+					fraseFinal = "";
+					break;
+				case 1:
+					fraseFinal = "Diez";
+					break;
+				case 2:
+					fraseFinal = "Veinte";
+					break;
+				case 3:
+					fraseFinal = "Treinta";
+					break;
+				case 4:
+					fraseFinal = "Cuarenta";
+					break;
+				case 5:
+					fraseFinal = "Cincuenta";
+					break;
+				case 6:
+					fraseFinal = "Sesenta";
+					break;
+				case 7:
+					fraseFinal = "Setenta";
+					break;
+				case 8:
+					fraseFinal = "Ochenta";
+					break;
+				case 9:
+					fraseFinal = "Noventa";
+					break;
+			}
+			if ((n > 10) && unidades > 0) 
+				fraseFinal = fraseFinal + " y "; 
+			else 
+				fraseFinal = fraseFinal + "";
+			
+			switch (unidades) {
+				case 0:
+					fraseFinal = fraseFinal + "";
+					break;
+				case 1:
+					fraseFinal = fraseFinal + "uno";
+					break;
+				case 2:
+					fraseFinal = fraseFinal + "dos";
+					break;
+				case 3:
+					fraseFinal = fraseFinal + "tres";
+					break;
+				case 4:
+					fraseFinal = fraseFinal + "cuatro";
+					break;
+				case 5:
+					fraseFinal = fraseFinal + "cinco";
+					break;
+				case 6:
+					fraseFinal = fraseFinal + "seis";
+					break;
+				case 7:
+					fraseFinal = fraseFinal + "siete";
+					break;
+				case 8:
+					fraseFinal = fraseFinal + "ocho";
+					break;
+				case 9:
+					fraseFinal = fraseFinal + "nueve";
+					break;
+	
+			}
+			return fraseFinal;
+		}
 	}
 
 	/*
@@ -80,7 +200,16 @@ public class Calculator {
 	 * dd-MM-yyyy
 	 */
 	public static boolean isLeapYear(String fecha) {
-		throw new NotImplementedException("no implementado");
+		if ((fecha == "")||(fecha == null)) 
+			return false;
+		else {
+			String añoString = fecha.substring(fecha.length() - 4);
+			int añoInt = Integer.parseInt(añoString);
+			if ((añoInt % 4 == 0) && ((añoInt % 100 != 0) || (añoInt % 400 == 0)))
+				return true;
+			else
+				return false;
+		}
 	}
 
 	/*
